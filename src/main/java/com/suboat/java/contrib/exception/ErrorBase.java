2b657154afdea405d226a0c9db666e3e32a2b46e
tudyzhb <tudyzhb@gmail.com>
2019-12-30 10:24:07 +0800

    feat: genereal rest error defined

---
+++ src/main/java/com/suboat/java/contrib/exception/ErrorBase.java
@@ -0,0 +1,39 @@
+package com.suboat.java.contrib.exception;
+
+import java.text.MessageFormat;
+
+public class ErrorBase extends RuntimeException {
+
+	protected Integer code = 0;
+
+	protected String prefix = "base";
+
+	protected String detail = "error";
+
+	protected Object[] arguments;
+
+	private String message;
+
+	public ErrorBase(Object... args) {
+		super("baseError");
+		this.arguments = args;
+	}
+
+	@Override
+	public String getMessage() {
+		if (this.message == null) {
+			if (!this.detail.contains("{0}") && this.arguments.length > 0) {
+				this.detail = this.detail + "|{0}";
+			}
+			MessageFormat txtFmt = new MessageFormat(this.detail);
+			this.message = MessageFormat.format("{0}{1}|", this.prefix, this.code) + txtFmt.format(this.arguments);
+		}
+		return this.message;
+	}
+
+	@Override
+	public Throwable fillInStackTrace() {
+		return this;
+	}
+
+}
