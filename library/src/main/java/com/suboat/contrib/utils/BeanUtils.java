8341ee0290db72f25bbc1e339e3aa24c59627f9c
tudyzhb <tudyzhb@gmail.com>
2020-01-08 09:02:21 +0800

    feat: copyPropertiesIgnoreNull方法

--- library/src/main/java/com/suboat/contrib/utils/BeanUtils.java
+++ library/src/main/java/com/suboat/contrib/utils/BeanUtils.java
@@ -1,8 +1,13 @@
 package com.suboat.contrib.utils;
 
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Bean工具
@@ -75,6 +80,27 @@ public class BeanUtils extends org.springframework.beans.BeanUtils {
 						}
 					}
 				});
+	}
+
+	// 查找空值字段
+	public static String[] getNullPropertyNames(Object source) {
+		final BeanWrapper src = new BeanWrapperImpl(source);
+		java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+		Set<String> emptyNames = new HashSet<String>();
+		for (java.beans.PropertyDescriptor pd : pds) {
+			Object srcValue = src.getPropertyValue(pd.getName());
+			if (srcValue == null)
+				emptyNames.add(pd.getName());
+		}
+
+		String[] result = new String[emptyNames.size()];
+		return emptyNames.toArray(result);
+	}
+
+	// 拷贝非空字段
+	public static void copyPropertiesIgnoreNull(Object src, Object target) {
+		BeanUtils.copyProperties(src, target, getNullPropertyNames(src));
 	}
 
 }
