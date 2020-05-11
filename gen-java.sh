c38ba9eab170602bd8cb9bdc6f47d54702490bb5
tudyzhb <tudyzhb@gmail.com>
2020-05-11 17:24:03 +0800

    fix: 去除无趣注释

--- gen-java.sh
+++ gen-java.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#thrift -r --gen java -out library/src/main/java/ rpcbase.thrift
-thrift -r --gen java -out library/src/main/java/ user.thrift
-thrift -r --gen java -out library/src/main/java/ wallet.thrift
-thrift -r --gen java -out library/src/main/java/ mall.thrift
+thrift -r --gen java:generated_annotations=suppress -out library/src/main/java/ user.thrift
+thrift -r --gen java:generated_annotations=suppress -out library/src/main/java/ wallet.thrift
+thrift -r --gen java:generated_annotations=suppress -out library/src/main/java/ mall.thrift
