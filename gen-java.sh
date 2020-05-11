8129304209fb098c1841ec343bfaf802c1e82824
tudyzhb <tudyzhb@gmail.com>
2020-05-11 17:17:56 +0800

    chore: 更新日期

--- gen-java.sh
+++ gen-java.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #thrift -r --gen java -out library/src/main/java/ rpcbase.thrift
 thrift -r --gen java -out library/src/main/java/ user.thrift
+thrift -r --gen java -out library/src/main/java/ wallet.thrift
+thrift -r --gen java -out library/src/main/java/ mall.thrift
