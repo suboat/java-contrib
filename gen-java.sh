da3c18cc9e2d57ed4171889ff2254f6d417c3906
tudyzhb <tudyzhb@gmail.com>
2020-05-11 17:26:14 +0800

    chore: 优化rpc脚本

--- gen-java.sh
+++ gen-java.sh
@@ -2,3 +2,5 @@
 thrift -r --gen java:generated_annotations=suppress -out library/src/main/java/ user.thrift
 thrift -r --gen java:generated_annotations=suppress -out library/src/main/java/ wallet.thrift
 thrift -r --gen java:generated_annotations=suppress -out library/src/main/java/ mall.thrift
+./format.sh
+git checkout HEAD -- library/src/main/java/com/suboat/contrib/rpc/base/Error.java
