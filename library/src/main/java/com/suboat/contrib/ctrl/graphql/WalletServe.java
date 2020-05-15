2e273cc87456dd6c136211d409d7c2452843a53e
wesker <wesker@suboat.com>
2020-05-15 14:07:27 +0800

    chore: 钱包模块

---
+++ library/src/main/java/com/suboat/contrib/ctrl/graphql/WalletServe.java
@@ -0,0 +1,90 @@
+package com.suboat.contrib.ctrl.graphql;
+
+import com.suboat.contrib.rpc.RpcConnConfig;
+import com.suboat.contrib.rpc.wallet.WalletService;
+import lombok.Data;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Data
+public class WalletServe {
+
+	private static final Logger log = LoggerFactory.getLogger(WalletServe.class);
+
+	// 默认公共参数
+	public static RpcConnConfig Config;
+
+	private TTransport transport;
+
+	private WalletService.Client client;
+
+	private RpcConnConfig config;
+
+	public WalletServe() {
+		syncConfig();
+	}
+
+	//
+	public WalletServe(RpcConnConfig cfg) {
+		config = cfg;
+		syncConfig();
+	}
+
+	//
+	public WalletServe(String addr) {
+		config = new RpcConnConfig();
+		config.host = addr;
+		config.timeout = 30000;
+		syncConfig();
+	}
+
+	private void syncConfig() {
+		if (Config == null && config != null) {
+			Config = config;
+		}
+		if (config == null && Config != null) {
+			config = Config;
+		}
+		if (config == null) {
+			config = new RpcConnConfig();
+			config.host = "localhost";
+			config.port = 8494;
+			config.timeout = 30000;
+		}
+	}
+
+	public WalletService.Client getClient() {
+		if (transport != null && client != null) {
+			return client;
+		}
+		try {
+			log.info("rpc-wallet conn {}:{} open", config.host, config.port);
+			transport = new TSocket(config.host, config.port, config.timeout);
+			// 协议要和服务端一致
+			TProtocol protocol = new TBinaryProtocol(transport);
+			transport.open();
+			client = new WalletService.Client(protocol);
+		}
+		catch (TTransportException e) {
+			e.printStackTrace();
+		}
+		return client;
+	}
+
+	// 关闭链接
+	public void close() {
+		if (transport == null) {
+			return;
+		}
+		log.info("rpc-wallet conn {}:{} close", config.host, config.port);
+		transport.close();
+		transport = null;
+		client = null;
+	}
+
+}
