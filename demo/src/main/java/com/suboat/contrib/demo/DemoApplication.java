896a817add62589843333f24db409a05a9e3a9af
tudyzhb <tudyzhb@gmail.com>
2021-04-08 16:47:19 +0800

    feat: rpc:msg

--- demo/src/main/java/com/suboat/contrib/demo/DemoApplication.java
+++ demo/src/main/java/com/suboat/contrib/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.suboat.contrib.demo;
 
 import com.suboat.contrib.MyService;
+import com.suboat.contrib.ctrl.graphql.MsgServe;
 import com.suboat.contrib.ctrl.graphql.UserServe;
 import com.suboat.contrib.rpc.RpcConnConfig;
 import org.slf4j.Logger;
@@ -26,15 +27,25 @@ public class DemoApplication {
 		UserServe.Config.host = "192.168.6.213";
 		UserServe.Config.port = 30709;
 		UserServe.Config.framed = true;
-		log.info("初始化配置 {}", UserServe.Config);
+		log.info("初始化配置 user: {}", UserServe.Config);
+		MsgServe.Config = new RpcConnConfig();
+		MsgServe.Config.host = "192.168.6.213";
+		MsgServe.Config.port = 32542;
+		MsgServe.Config.framed = true;
+		log.info("初始化配置 msg: {}", UserServe.Config);
 
 		// rpc连接
 		UserServe userServe = null;
+		MsgServe msgServe = null;
 		try {
 			// 用户模块
 			userServe = new UserServe();
 			log.info("rpc-user version get");
 			log.info(String.valueOf(userServe.getClient().version()));
+			// 消息模块
+			msgServe = new MsgServe();
+			log.info("rpc-msg version get");
+			log.info(String.valueOf(msgServe.getClient().version()));
 		}
 		catch (Exception e) {
 			e.printStackTrace();
@@ -42,6 +53,9 @@ public class DemoApplication {
 		finally {
 			if (userServe != null) {
 				userServe.close();
+			}
+			if (msgServe != null) {
+				msgServe.close();
 			}
 		}
 	}
--- gen-java.sh
+++ gen-java.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 thrift -r --gen java:generated_annotations=suppress -out library/src/main/java/ user.thrift
 thrift -r --gen java:generated_annotations=suppress -out library/src/main/java/ wallet.thrift
+thrift -r --gen java:generated_annotations=suppress -out library/src/main/java/ msg.thrift
 thrift -r --gen java:generated_annotations=suppress -out library/src/main/java/ mall.thrift
 ./format.sh
 git checkout HEAD -- library/src/main/java/com/suboat/contrib/rpc/base/Error.java
